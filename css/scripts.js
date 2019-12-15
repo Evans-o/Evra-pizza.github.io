b33c57a35a0a8da0bdedd345891f92005b337437
Evans-o <evansopindi@yahoo.com>
2019-12-15 16:39:37 +0300

    added initial structures

--- css/scripts.js
+++ css/scripts.js
@@ -1,5 +1,1 @@
-<input type="button" onclick="myFunction()" value="Send order">
-        <br><br>
-        <input type="text" id="order"  size="80">
-        <input type="submit" id="submit" onclick="pizza()" value="Sbmit"><br>
-        
+
